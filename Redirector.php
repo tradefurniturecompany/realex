d053378223dc233c75d1079a7cf1a1e45b92ffc8
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-07 12:23:59 +0000

    https://github.com/tradefurniturecompany/realex/issues/1

---
+++ Redirector.php
@@ -0,0 +1,9 @@
+<?php
+namespace TFC\Realex;
+# 2022-12-07
+# «If the customer navigates back from the bank card payment page,
+# the cart contents should be restored, and the customer should be redirected back to the Magento checkout page»:
+# https://github.com/tradefurniturecompany/realex/issues/1
+final class Redirector {
+
+}
