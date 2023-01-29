744cbf13da94dd33778f3a6ecb6d63ec7ef7e40a
Dmitry Fedyuk <dfediuk@gmail.com>
2023-01-29 22:15:04 +0000

    https://github.com/tradefurniturecompany/realex/issues/1

--- Observer/Predispatch/Departure.php
+++ Observer/Predispatch/Departure.php
@@ -7,6 +7,9 @@ use TFC\Realex\Redirector as R;
 final class Departure implements IO {
 	/**
 	 * 2023-01-30
+	 * @override
+	 * @see IO::execute()
+	 * @used-by \Magento\Framework\Event\Invoker\InvokerDefault::_callObserverMethod()
 	 */
 	function execute(O $o):void {
 		R::set();
